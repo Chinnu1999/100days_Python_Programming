4362ea7bc2727413b08cc73bc3ad11ec8c7a7084
Chinnu <94435318+Chinnu1999@users.noreply.github.com>
2021-11-25 19:11:54 +0530

    Update Day2.py

--- Python/Day2.py
+++ Python/Day2.py
@@ -149,29 +149,9 @@ print(f"You have {days} days, {weeks} weeks, and {months} months left.")
 
 #----------------------------------------------------------------------------------------------------
 
-#Excercise
-
-print("Welcome to the tip calculator.")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
-p_bill = int(input("How many people to split the bill? "))
-b_tip = tip / 100
-t_tip = bill * b_tip
-total = bill + t_tip
-person_bill = total / p_bill
-tips = round(person_bill, 2)
-print(total)
-print(f"Each person should pay: ${tips}")
 
 
 
-print("Welcome to the tip calculator.")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
-people = int(input("How many people to split the bill? "))
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
-print(f"Each person should pay: ${final_amount}")
+
+
+
