7626625b9aa55bbecfe645996db004957b20d223
Chinnu <94435318+Chinnu1999@users.noreply.github.com>
2022-04-27 11:45:33 +0530

    Create Day6.py

---
+++ Python/Day6/Day6.py
@@ -0,0 +1,33 @@
+                                                                   #Python Programming 
+                                                                         # Day-6
+
+# Defining and Calling Python Function
+
+# create or define a function
+def my_fucntion(): # def -> keyword, my_function -> function name with paranthesis and colon
+  print("Hello World!.")
+  print("Bye")
+
+my_fucntion()   # calling the function
+# A function is a block of code which only runs when it is called.
+
+
+# A Simple Robot game
+
+def turn_right():   # instead of using turn left 3 times we use function as turn right()
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()     # turn robot left
+move()          # move forward  
+turn_right()    # turn left 3 times
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_left()
+turn_left()
+
+#-------------------------------------------------------------------------------------------------------------------------------------
